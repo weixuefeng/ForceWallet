8540a1866e296096693475fbe22059f16e86e1a2
weixuefeng <weixuefeng1018@gmail.com>
2021-12-29 20:14:34 +0800

    update page

--- app/src/main/java/org/newtonproject/forcewallet/MainActivity.kt
+++ app/src/main/java/org/newtonproject/forcewallet/MainActivity.kt
@@ -10,13 +10,13 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import org.newtonproject.forcewallet.ui.theme.ForseWalletTheme
+import org.newtonproject.forcewallet.ui.theme.ForceWalletTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ForseWalletTheme {
+            ForceWalletTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -37,7 +37,7 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ForseWalletTheme {
+    ForceWalletTheme {
         Greeting("Android")
     }
 }
